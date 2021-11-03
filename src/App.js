8c9188b138ee1300dd0c8e14a7293e2af3db2fae
yanalshmilan <yshmilan@gmail.com>
2021-11-03 23:14:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Signin from './components/Auth/Signin';
+import Signup from './components/Auth/Signup';
+import Chat from './components/Chat/Chat';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Chat App</h1>
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route exact path="/" element={<Chat />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Auth/Signin.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Signin() {
+  return (
+    <div>
+      <h1>Signin screen</h1>
+    </div>
+  );
+}
